adfe491c41ad72c1b70195504bd0692f809864e4
mansk <mansurulkarim@gmail.com>
2024-03-21 16:45:10 +0000

    Add take_turn method to Battle class

--- test/test_battle.py
+++ test/test_battle.py
@@ -1,6 +1,6 @@
 from src.battle import Battle
-from src.pokemon import Pokemon
-import pytest
+from src.pokemon import *
+import pytest, types
 
 @pytest.fixture
 def pikachu():
@@ -10,23 +10,31 @@ def pikachu():
 def flareon():
     return ["Flareon", 65, 20, "Fire blast"]
 
+@pytest.fixture
+def leafeon():
+    return ["Leafeon", 65, 17, "Giga drain"]
+
+@pytest.fixture
+def vaporeon():
+    return ["Vaporeon", 70, 19, "Hydro pump"]
+
 
 @pytest.mark.describe('Attribute Tests')
 class TestAttributes:    
     @pytest.mark.it('Has pokemon_1, pokemon_2 and take_turn attributes')
     def test_battle_has_required_attributes(self, pikachu, flareon):
-        test_pokemon_1 = Pokemon(*pikachu)
-        test_pokemon_2 = Pokemon(*flareon)
+        test_pokemon_1 = NormalPokemon(*pikachu)
+        test_pokemon_2 = FirePokemon(*flareon)
         test_battle = Battle(test_pokemon_1, test_pokemon_2)
 
-        assert isinstance(test_battle.pokemon_1, Pokemon)
-        assert isinstance(test_battle.pokemon_2, Pokemon)
+        assert isinstance(test_battle.pokemon_1, NormalPokemon)
+        assert isinstance(test_battle.pokemon_2, FirePokemon)
         assert isinstance(test_battle.current_turn, int)
 
     @pytest.mark.it('Pokemon instances in Battle are same as those passed to constructor')
     def test_battle_initialises_with_passed_pokemon_instances(self, pikachu, flareon):
-        test_pokemon_1 = Pokemon(*pikachu)
-        test_pokemon_2 = Pokemon(*flareon)
+        test_pokemon_1 = NormalPokemon(*pikachu)
+        test_pokemon_2 = FirePokemon(*flareon)
         test_battle = Battle(test_pokemon_1, test_pokemon_2)
 
         assert test_battle.pokemon_1 is test_pokemon_1
@@ -34,10 +42,97 @@ class TestAttributes:
     
     @pytest.mark.it('Check current_turn initialises to 1')
     def test_battle_initiliases__current_turn__to_1(self, pikachu, flareon):
-        test_pokemon_1 = Pokemon(*pikachu)
-        test_pokemon_2 = Pokemon(*flareon)
+        test_pokemon_1 = NormalPokemon(*pikachu)
+        test_pokemon_2 = FirePokemon(*flareon)
         test_battle = Battle(test_pokemon_1, test_pokemon_2)
+
+        assert test_battle.current_turn == 0
+
+    @pytest.mark.it('Check Battle initialises respective damage values')
+    def test_battle_initialises_damage_values(self, pikachu, flareon):
+        test_pokemon_1 = GrassPokemon(*pikachu)
+        test_pokemon_2 = FirePokemon(*flareon)
+
+        test_battle = Battle(test_pokemon_1, test_pokemon_2)
+
+        # multiplier 0.5 * default damage 20
+        assert test_battle.pokemon_1_damage == 10
+        # multiplier 1.5 * default damage 20
+        assert test_battle.pokemon_2_damage == 30
+
+
+@pytest.mark.describe('take_turn Method Tests')
+class TestTakeTurn:    
+    @pytest.mark.it('take_turn is a method')
+    def test_battle_has__take_turn__method(self, pikachu, flareon):
+        test_pokemon_1 = NormalPokemon(*pikachu)
+        test_pokemon_2 = FirePokemon(*flareon)
+        test_battle = Battle(test_pokemon_1, test_pokemon_2)
+
+        assert isinstance(test_battle.take_turn, types.MethodType)
+
+    @pytest.mark.it('Pokemon 1 deals damage to pokemon 2 with take_turn')
+    def test_battle__take_turn__deals_damage(self, pikachu, flareon):
+        test_pokemon_1 = NormalPokemon(*pikachu)
+        test_pokemon_2 = FirePokemon(*flareon)
+        test_battle = Battle(test_pokemon_1, test_pokemon_2)
+
+        test_battle.take_turn()
+
+        assert test_pokemon_2.hit_points == 45
+
+    @pytest.mark.it('Pokemon 2 hit points changes after pokemon 1 takes turn')
+    def test_battle__take_turn__changes_value_of_hit_points(self, pikachu, flareon):
+        test_pokemon_1 = NormalPokemon(*pikachu)
+        test_pokemon_2 = FirePokemon(*flareon)
+        test_battle = Battle(test_pokemon_1, test_pokemon_2)
+
+        pokemon_2_initial_hit_points = test_pokemon_2.hit_points
+
+        test_battle.take_turn()
+
+        assert test_pokemon_2.hit_points < pokemon_2_initial_hit_points
+
+    @pytest.mark.it('Pokemon 1 deals different damage to pokemon 2 with different combination of pokemon')
+    def test_battle__take_turn__deals_different_damage(self, leafeon, vaporeon):
+        test_pokemon_1 = GrassPokemon(*leafeon)
+        test_pokemon_2 = WaterPokemon(*vaporeon)
+        test_battle = Battle(test_pokemon_1, test_pokemon_2)
+
+        test_battle.take_turn()
+
+        assert test_pokemon_2.hit_points == 44.5
+
+    @pytest.mark.it('Current turn changes after turn has been taken')
+    def test_battle__current_turn__attribute_is_updated_after__take_turn__called(self, leafeon, vaporeon):
+        test_pokemon_1 = GrassPokemon(*leafeon)
+        test_pokemon_2 = WaterPokemon(*vaporeon)
+        test_battle = Battle(test_pokemon_1, test_pokemon_2)
+
+        test_battle.take_turn()
 
         assert test_battle.current_turn == 1
 
-    
+    @pytest.mark.it('Current turn reverts to Pokemon 1 after two turns taken')
+    def test_battle__current_turn__attribute_switches_back_to_0_after__take_turn__called_twice(self, leafeon, vaporeon):
+        test_pokemon_1 = GrassPokemon(*leafeon)
+        test_pokemon_2 = WaterPokemon(*vaporeon)
+        test_battle = Battle(test_pokemon_1, test_pokemon_2)
+
+        test_battle.take_turn()
+        test_battle.take_turn()
+
+        assert test_battle.current_turn == 0
+
+    @pytest.mark.it('Battle raises exception when defending pokemon has already fainted')
+    def test_battle__take_turn__against_fainted_pokemon_raises_exception(self):
+        test_pokemon_1 = FirePokemon('Firestrong', 100, 40, 'Inferno')
+        test_pokemon_2 = GrassPokemon('Weakgrass', 60, 10, 'Wilt')
+        test_battle = Battle(test_pokemon_1, test_pokemon_2)
+
+        test_battle.take_turn()
+
+        with pytest.raises(Exception) as exc_info:
+            test_battle.take_turn()
+
+        assert "has already fainted" in str(exc_info.value)
