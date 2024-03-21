c897b38e4409cdde0a111ad2668a5d51d9704a5b
mansk <mansurulkarim@gmail.com>
2024-03-21 14:40:14 +0000

    Create Pokeball class with initial attributes and methods

---
+++ src/pokeball.py
@@ -0,0 +1,18 @@
+from src.pokemon import *
+
+class Pokeball():
+    def __init__(self):
+        self.__current_pokemon = None
+
+    def catch(self, pokemon):
+        if self.__current_pokemon:
+            raise Exception(f'Pokeball already contains pokemon')
+        
+        self.__current_pokemon = pokemon
+
+    @property
+    def current_pokemon(self):
+        return self.__current_pokemon
+    
+    def is_empty(self):
+        return not self.__current_pokemon
---
+++ test/test_pokeball.py
@@ -0,0 +1,79 @@
+from src.pokemon import Pokemon, FirePokemon, GrassPokemon, WaterPokemon, NormalPokemon
+from src.pokeball import Pokeball
+import pytest, types
+
+@pytest.fixture
+def pikachu():
+    return ['Pikachu', 50, 20, 'Lightning bolt']
+
+@pytest.fixture
+def flareon():
+    return ["Flareon", 65, 20, "Fire blast"]
+
+@pytest.fixture
+def leafeon():
+    return ["Leafeon", 65, 17, "Giga drain"]
+
+@pytest.fixture
+def vaporeon():
+    return ["Vaporeon", 70, 19, "Hydro pump"]
+
+@pytest.fixture
+def eevee():
+    return ["Eevee", 55, 18, "Headbutt"]
+
+
+@pytest.mark.describe('Tests for Pokeball class')
+class TestPokeball:
+    def test_pokeball_initialises_with_no_current_pokemon(self):
+        test_pokeball = Pokeball()
+
+        assert test_pokeball.current_pokemon == None
+
+    def test_pokeball_has_catch_method(self):
+        test_pokeball = Pokeball()
+
+        assert isinstance(test_pokeball.catch, types.MethodType)
+
+    def test_pokeball_catch_method_captures_pokemon(self, pikachu):
+        test_pikachu = NormalPokemon(*pikachu)
+        test_pokeball = Pokeball()
+
+        test_pokeball.catch(test_pikachu)
+
+        assert test_pokeball.current_pokemon is test_pikachu
+
+    def test_pokeball_raises_exception_when_catch_called_with_pokeball_filled(self, pikachu):
+        test_pikachu_1 = NormalPokemon(*pikachu)
+        test_pikachu_2 = NormalPokemon(*pikachu)
+        test_pokeball = Pokeball()
+
+        test_pokeball.catch(test_pikachu_1)
+
+        with pytest.raises(Exception) as exc_info:
+            test_pokeball.catch(test_pikachu_2)
+
+        assert "Pokeball already contains pokemon" in str(exc_info.value)
+
+    def test_pokeball_has__is_empty__method(self):
+        test_pokeball = Pokeball()
+
+        assert isinstance(test_pokeball.is_empty, types.MethodType)
+
+    def test_pokeball__is_empty__method_returns_boolean(self):
+        test_pokeball = Pokeball()
+
+        assert isinstance(test_pokeball.is_empty(), bool)
+    
+    def test_pokeball__is_empty__returns_true_when_pokeball_empty(self):
+        test_pokeball = Pokeball()
+
+        assert test_pokeball.is_empty()
+
+    def test_pokeball__is_empty__returns_false_when_pokeball_contains_pokemon(self, pikachu):
+        test_pokemon = NormalPokemon(*pikachu)
+        test_pokeball = Pokeball()
+        test_pokeball.catch(test_pokemon)
+
+        assert test_pokeball.is_empty() is False
+
